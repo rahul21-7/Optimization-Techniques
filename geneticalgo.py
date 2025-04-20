c1faba0ed366fd1d462857a5a092ada88bca5a1a
rahul21-7 <dasari2007rahul2007@gmail.com>
2025-04-20 21:32:55 +0530

    Added a few more files

--- geneticalgo.py
+++ geneticalgo.py
@@ -1,1 +1,9 @@
 import numpy as np
+import genvars as gv
+
+def geneticAlgorithm(population_size, max_iterations,):
+    gv.size_of_population = population_size
+    initial_random_population = gv.initial_random_population
+    population = initial_random_population
+    for i in range(max_iterations):
+        dec
--- genvars.py
+++ genvars.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import random as random
 
-size_of_population = random.randint(1,1000)
+# size_of_population = random.randint(1,1000)
+size_of_population = int
 
-n = int
 
-initial_random_population = np.random.rand(size_of_population,n)
+initial_random_population = np.random.rand(size_of_population)
 
 # print((initial_random_population*10).astype(int))
 
