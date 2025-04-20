fa99628d2ea447ec035f0bc461dfe41fdf83c8d9
rahul21-7 <dasari2007rahul2007@gmail.com>
2025-04-20 18:28:15 +0530

    Seperated files for functions and variables

---
+++ genvars.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import random as random
+
+size_of_population = random.randint(1,1000)
+
+n = int
+
+initial_random_population = np.random.rand(size_of_population,n)
+
+# print((initial_random_population*10).astype(int))
+
+
